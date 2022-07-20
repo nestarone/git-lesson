23ccd10a7b0614aebf44c051ef6a62809de9be5f
nestarone <nestarone@gmail.com>
2022-07-20 16:15:36 +0700

    fix pk for tanle TodoListItems

---
+++ ToDoList/Infrastructure/Migrations/20220720085351_fixUserFK.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Infrastructure.Database.Migrations
+{
+    public partial class fixUserFK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            delete from ""TodoListItem"" where ""UserId"" is null; 
+            ");
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoListItem_Users_UserId",
+                table: "TodoListItem");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "TodoListItem",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoListItem_Users_UserId",
+                table: "TodoListItem",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoListItem_Users_UserId",
+                table: "TodoListItem");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "TodoListItem",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoListItem_Users_UserId",
+                table: "TodoListItem",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
